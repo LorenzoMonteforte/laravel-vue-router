62f3f7c7fcafc25a457e4959602d5914917c5bc6
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-02-07 17:04:27 +0100

    Punto 5

--- src/store.js
+++ src/store.js
@@ -1,5 +1,14 @@
 import { reactive } from 'vue'
+import axios from 'axios'; //importo Axios
 
 export const store = reactive({
-    chiave: "valore"
+    methods : {
+        myAxios : function (){
+            axios.get("http://127.0.0.1:8000/api/api2").then(risultato => {
+            console.log(risultato);
+            }).catch(errore => {
+            console.error(errore);
+            });
+        }
+    }
 });
