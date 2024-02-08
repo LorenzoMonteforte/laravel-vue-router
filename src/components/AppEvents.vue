5b743ad33e6d17d1c3c0cd010b6c8e19914b2365
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2024-02-08 16:57:52 +0100

    Eventi (Punto 3)

--- src/components/AppEvents.vue
+++ src/components/AppEvents.vue
@@ -1,11 +1,27 @@
 <script>
+import { store } from "../store"
+
 export default {
-    name : "AppEvents"
+    name : "AppEvents",
+    data() {
+		return {
+			store
+		}
+	}
 }
 </script>
 
 <template>
     <h1>Events</h1>
+    <ul v-for="event in store.events">
+        <li>Nome: {{ event.name }}</li>
+        <li>Descrizione: {{ event.description }}</li>
+        <li>Città: {{ event.city }}</li>
+        <li>Data: {{ event.date }}</li>
+        <ul>
+            <li v-for="tag in event.tags">{{ tag.name }}</li>
+        </ul>
+    </ul>
 </template>
 
 <style scoped>
--- src/store.js
+++ src/store.js
@@ -2,10 +2,24 @@ import { reactive } from 'vue'
 import axios from 'axios'; //importo Axios
 
 export const store = reactive({
+    events: [],
     methods : {
         myAxios : function (){
             axios.get("http://127.0.0.1:8000/api/api2").then(risultato => {
-            console.log(risultato);
+            for(let i=0; i<risultato.data.payload.length; i++){
+                store.events[i] = {
+                    name : risultato.data.payload[i].name,
+                    description : risultato.data.payload[i].description,
+                    city : risultato.data.payload[i].city,
+                    date : risultato.data.payload[i].date,
+                    tags : []
+                };
+                for(let index=0; index<risultato.data.payload[i].tags.length; index++){
+                    store.events[i].tags[index] = {
+                        name : risultato.data.payload[i].tags[index].name
+                    };
+                }
+            }
             }).catch(errore => {
             console.error(errore);
             });
